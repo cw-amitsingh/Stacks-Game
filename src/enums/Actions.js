9915a0017767aba1ed1462bd5073463a2adbfcf4
cw-amitsingh <51703039+cw-amitsingh@users.noreply.github.com>
2020-08-15 20:02:03 +0530

    added first impression of the game

---
+++ src/enums/Actions.js
@@ -0,0 +1,5 @@
+export const Actions = {
+  SetRowPosition: "set_row_position",
+  AddNewRow: "add_new_row",
+  Restart: "restart",
+};
---
+++ src/styles/colors.js
@@ -0,0 +1,5 @@
+const colors = {
+  pink: "#FF0EF9"
+};
+
+export default colors;
